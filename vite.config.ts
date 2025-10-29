b9a0bc0efb7700d7fe7dd7c03a7bcfda50ea8e99
Christian Heins <129910735+christianheins@users.noreply.github.com>
2025-10-30 00:47:19 +0100

    connect3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/invitation/",  // 👈 Add this line
   server: {
     host: "::",
     port: 8081,
